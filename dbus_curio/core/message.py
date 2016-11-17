fd92344ac9916d327093c7d331b948840c23e88f
Hugo Ribeiro <hugo.ribeiro@net.com.br>
2016-11-17 19:22:38 -0200

    flake

--- dbus_curio/core/message.py
+++ dbus_curio/core/message.py
@@ -1,15 +1,13 @@
 from collections import namedtuple
 from enum import Enum, IntEnum
 
-HeaderField = namedtuple('HeaderField',
-    [
+HeaderField = namedtuple('HeaderField', [
         'code',
         'value'
     ]
 )
 
-Header = namedtuple('Header',
-    [
+Header = namedtuple('Header', [
         'endianness',
         'message_type',
         'flags',
@@ -38,11 +36,11 @@ class Flags(IntEnum):
     NO_REPLY_EXPECTED = 0x1
     NO_AUTO_START = 0x2
     ALLOW_INTERACTIVE_AUTHORIZATION = 0x4
-    
+
     @classmethod
     def all(cls):
         return 0x7
-    
+
     def all_but(self):
         return Flags.all() ^ self
 
@@ -50,14 +48,14 @@ class Flags(IntEnum):
 class HeaderFieldCodes(IntEnum):
     INVALID = 0
     PATH = 1
-    INTERFACE =	2
-    MEMBER =3
+    INTERFACE = 2
+    MEMBER = 3
     ERROR_NAME = 4
     REPLY_SERIAL = 5
     DESTINATION = 6
     SENDER = 7
     SIGNATURE = 8
     UNIX_FDS = 9
-    
+
     def new(self, value):
         return HeaderField(self.value, value)
