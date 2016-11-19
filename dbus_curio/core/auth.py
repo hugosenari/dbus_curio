9c49fee71a281578f0a8181a1ce2cf32b675c1bd
hugosenari <hugosenari@gmail.com>
2016-11-19 15:07:11 -0200

    implement other auth methods

--- dbus_curio/core/auth.py
+++ dbus_curio/core/auth.py
@@ -1,9 +1,56 @@
-from os import getpid
-from binascii import hexlify
+from getpass import getuser
+from binascii import hexlify, unhexlify
+from hashlib import sha1
+from os import urandom, path
 
+COOKIE_DIR = '~/.dbus-keyrings'
 
 class AuthException(Exception):
     pass
+
+
+def _dbus_gookie(context, uid, challenge):
+    file_name = path.expanduser(path.join(COOKIE_DIR, context))
+    pass_ = hexlify(sha1(urandom(8)).digest())
+    try:
+        with file_ in open(file_name, r):
+            for line in file_:
+                _uid, _time, _cookie = line.split()
+                if uid == _uid:
+                    result = hexlify(
+                        sha1(
+                            b':'.join(
+                                [challenge, pass_, _cookie]
+                            )
+                        ).digest()
+                    )
+                    return b' '.join([pass_, result])
+            raise AuthException('DBUS_COOKIE_SHA1 cookie not found')
+    except IOError as e:
+        raise AuthException('DBUS_COOKIE_SHA1 cannot read cookie file', e)
+
+
+def anonymous():
+    user = hexlify(getuser().encode())
+    resp = yield b'AUTH ANONYMOUS ' + user
+    if b'DATA' in resp:
+        resp = yield b'DATA'
+    if b'OK ' in resp:
+        resp = yield b'NEGOTIATE_UNIX_FD'
+        resp = yield b'BEGIN'
+    raise AuthException('Cannot authenticate with ANONYMOUS')
+
+
+def dbus_cookie_sha1():
+    user = hexlify(getuser().encode())
+    resp = yield b'AUTH DBUS_COOKIE_SHA1 ' + user
+    if b'DATA' in resp:
+        data = unhexlify(resp.strip(b'DATA').strip())
+        resp = yield b'DATA ' + hexlify(_dbus_gookie(*data.split()))
+    if b'OK ' in resp:
+        resp = yield b'NEGOTIATE_UNIX_FD'
+        resp = yield b'BEGIN'
+    raise AuthException('DBUS_COOKIE_SHA1 not implemented')
 
 
 def external():
@@ -16,42 +63,31 @@ def external():
     raise AuthException('Cannot authenticate with EXTERNAL')
 
 
-def anonymous():
-    pid = hexlify(str(getpid()).encode())
-    resp = yield b'AUTH ANONYMOUS ' + pid
-    if b'DATA' in resp:
-        resp = yield b'DATA'
-    if b'OK ' in resp:
-        resp = yield b'BEGIN'
-    raise AuthException('Cannot authenticate with ANONYMOUS')
-
-
 AUTH_METHODS = {
+    b'external': external,
     b'anonymous': anonymous,
-    b'external': external
+    b'dbus_cookie_sha1': dbus_cookie_sha1
 }
 
 
-def auths(methods=AUTH_METHODS):
+def auths(methods=None):
+    _methods = methods or AUTH_METHODS
     response = yield b'AUTH\r\n'
-    if not methods:
+    if not _methods:
         raise AuthException('No auth method implementation defined')
     server_methods = response.strip().split(b' ')[1:]
     if not server_methods:
         raise AuthException('Server has no auth methods')
     for server_method in server_methods:
-        method = methods.get(server_method.lower())
+        method = _methods.get(server_method.lower())
         try:
             if method:
                 gen = method()
                 command = next(gen)
                 while True:
                     response = yield command + b'\r\n'
-                    if b'BEGIN' in command:
-                        break
                     command = gen.send(response.strip())
-                else:
-                    break
         except AuthException:
             pass
-    raise AuthException('Cannot authenticate with defined methods')
+    raise AuthException('Cannot authenticate with defined methods ({})'
+                        .format(_methods.keys()))
