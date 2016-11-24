9cd045928491c4344d87ce0e2d665cd4d92da56d
hugosenari <hugosenari@gmail.com>
2016-11-24 12:16:07 -0200

    inverted uint64 to be first part of body so we can find body start by \xff\xff\xff\xff\xff\xff\xff\xff

--- sandbox/serialize.c
+++ sandbox/serialize.c
@@ -11,31 +11,32 @@ int main()
     guchar* mbuf;
     GError* error;
     // https://github.com/GNOME/glib/blob/f924d0b1f7d2b019f1abb56685dcfda74266c608/gio/tests/gdbus-serialization.c
-    GVariant *body = g_variant_new ("(sogybnqiuxtd)",
-    //    value 0:    string:      `this is a string'\n
-                          "this is a string",
-    //    value 1:    object_path: `/this/is/a/path'\n
-                          "/this/is/a/path",
-    //    value 2:    signature:   `sad'\n
-                          "sad",
-    //    value 3:    byte:         0x2a\n
-                          42,
-    //    value 4:    bool:         true\n
-                          TRUE,
-    //    value 5:    int16:        -42\n
-                          -42,
-    //    value 6:    uint16:       60000\n
-                          60000,
-    //    value 7:    int32:        -44\n
-                          -44,
-    //    value 8:    uint32:       100000\n
-                          100000,
-    //    value 9:    int64:        -34359738368\n
-                          -G_GINT64_CONSTANT(2)<<34,
-    //    value 10:   uint64:       18446744073709551615\n
+    GVariant *body = g_variant_new ("(tsogybnqiuxd)",
+    //    value 0:   uint64:       18446744073709551615\n
                           G_GUINT64_CONSTANT(0xffffffffffffffff),
+    //    value 1:    string:      `this is a string'\n
+                          "this is a string",
+    //    value 2:    object_path: `/this/is/a/path'\n
+                          "/this/is/a/path",
+    //    value 3:    signature:   `sad'\n
+                          "sad",
+    //    value 4:    byte:         0x2a\n
+                          42,
+    //    value 5:    bool:         true\n
+                          TRUE,
+    //    value 6:    int16:        -42\n
+                          -42,
+    //    value 7:    uint16:       60000\n
+                          60000,
+    //    value 8:    int32:        -44\n
+                          -44,
+    //    value 9:    uint32:       100000\n
+                          100000,
+    //    value 10:   int64:        -34359738368\n
+                          -G_GINT64_CONSTANT(2)<<34,
     //    value 11:   double:       42.500000\n";
-                          42.5);
+                          42.5
+    );
     error = NULL;
     message = g_dbus_message_new_signal(
                                         "/org/gtk/GDBus/TestObject",
@@ -53,5 +54,6 @@ int main()
     for (int i = 0; i < size; i++) {
         g_print("\\x%02x", mbuf[i]);
     }
+    g_print("\n");
     return 0;
 }
