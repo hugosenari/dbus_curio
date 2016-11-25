4100afa50abe7fe5fca0fe6468c479a7b6e26ada
Hugo Ribeiro <hugo.ribeiro@net.com.br>
2016-11-25 19:44:01 -0200

    resolve glib-2 lib

--- sandbox/inotify.sh
+++ sandbox/inotify.sh
@@ -1,1 +1,1 @@
-while inotifywait -e close_write serialize.c; do echo `g++ -g serialize.c -o serialize -I/usr/include/glib-2.0 -I/usr/lib/glib-2.0/include -Wall -Wextra -lglib-2.0 -lgio-2.0 -lgobject-2.0 -lgthread-2.0  -fpermissive`; done
+while inotifywait -e close_write serialize.c; do g++ -g serialize.c -o serialize `pkg-config --cflags glib-2.0` -Wall -Wextra -lglib-2.0 -lgio-2.0 -lgobject-2.0 -lgthread-2.0  -fpermissive; done
