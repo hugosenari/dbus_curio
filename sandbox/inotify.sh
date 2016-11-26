d34d3c954c42fd1362ec3332035d27098d737d44
hugosenari <hugosenari@gmail.com>
2016-11-26 18:24:03 -0200

    update serialize doc

--- sandbox/inotify.sh
+++ sandbox/inotify.sh
@@ -1,1 +1,17 @@
-while inotifywait -e close_write serialize.c; do g++ -g serialize.c -o serialize `pkg-config --cflags glib-2.0` -Wall -Wextra -lglib-2.0 -lgio-2.0 -lgobject-2.0 -lgthread-2.0  -fpermissive; done
+while inotifywait -e close_write serialize.c; do
+    g++ -g serialize.c -o serialize \
+        `pkg-config --cflags glib-2.0` \
+        -Wall -Wextra -lglib-2.0 -lgio-2.0 -lgobject-2.0 -lgthread-2.0  -fpermissive;
+#    export GLIB_PATH=/home/hugosenari/codes/glib;
+#    g++ -g serialize.c -o serialize_dbg \
+#        -I$GLIB_PATH -I$GLIB_PATH/glib -I$GLIB_PATH/gio -I$GLIB_PATH/gmodule -I$GLIB_PATH/gobject -I$GLIB_PATH/gthread \
+#        -Wall -Wextra -lglib-2.0 -lgio-2.0 -lgobject-2.0 -lgthread-2.0  -fpermissive;
+done
+
+#sh -c "
+#while inotifywait -e close_write gio/gdbusmessage.c; do
+#make;
+#g++ -g $PWD/../dbus_curio/sandbox/serialize.c -o $PWD/../dbus_curio/sandbox/serialize_dbg \
+#    -I$PWD -I$PWD/gio -I$PWD/gio/libs/ -I$PWD/glib -I$PWD/gmodule -I$PWD/gobject -I$PWD/gthread \
+#    -Wall -Wextra -lglib-2.0 -lgio-2.0 -lgobject-2.0 -lgthread-2.0  -fpermissive;
+#done"
--- sandbox/serialize.c
+++ sandbox/serialize.c
@@ -7,35 +7,35 @@
 int main()
 {
     GDBusMessage *message;
-    gsize size;
+    gsize size = G_GINT64_CONSTANT(0);
     guchar* mbuf;
     GError* error;
     // https://github.com/GNOME/glib/blob/f924d0b1f7d2b019f1abb56685dcfda74266c608/gio/tests/gdbus-serialization.c
-    GVariant *body = g_variant_new ("(tsogybnqiuxd)",
-    //    value 0:   uint64:       18446744073709551615\n
+    GVariant *body = g_variant_new ("(tyb)",
+    //    value 0:   uint64:       18446744073709551615
                           G_GUINT64_CONSTANT(0xffffffffffffffff),
-    //    value 1:    string:      `this is a string'\n
-                          "this is a string",
-    //    value 2:    object_path: `/this/is/a/path'\n
-                          "/this/is/a/path",
-    //    value 3:    signature:   `sad'\n
-                          "sad",
-    //    value 4:    byte:         0x2a\n
-                          42,
-    //    value 5:    bool:         true\n
-                          TRUE,
-    //    value 6:    int16:        -42\n
-                          -42,
-    //    value 7:    uint16:       60000\n
-                          60000,
-    //    value 8:    int32:        -44\n
-                          -44,
-    //    value 9:    uint32:       100000\n
-                          100000,
-    //    value 10:    int64:        -34359738368\n
-                          -G_GINT64_CONSTANT(2)<<34,
-    //    value 11:   double:       42.500000\n";
-                          42.5
+    //    value 1:    string:      `this is a string'
+    //                      "this is a string",
+    //    value 2:    object_path: `/this/is/a/path'
+    //                    "/this/is/a/path",
+    //    value 3:    signature:   `sad'
+//                        "sad",
+    //    value 4:    byte:         0x2a
+                          0xFF,
+    //    value 5:    bool:         true
+                          TRUE
+    //    value 6:    int16:        -42
+//                          0xFFFF
+//    //    value 7:    uint16:       60000
+//                          60000,
+//    //    value 8:    int32:        -44
+//                          -44,
+//    //    value 9:    uint32:       100000
+//                          100000,
+//    //    value 10:    int64:        -1
+//                          -G_GINT64_CONSTANT(1),
+//    //    value 11:   double:       42.500000;
+//                          42.5
     );
     error = NULL;
     message = g_dbus_message_new_signal(
@@ -44,15 +44,18 @@ int main()
                                         "cccccccccccccccccc");
     g_dbus_message_set_serial (message, 0x40);
     g_dbus_message_set_byte_order (message, G_DBUS_MESSAGE_BYTE_ORDER_LITTLE_ENDIAN);
-    g_dbus_message_set_header (message, G_DBUS_MESSAGE_HEADER_FIELD_SIGNATURE, g_variant_new_signature ("(sogybnqiuxtd)"));
+    g_dbus_message_set_header (message, G_DBUS_MESSAGE_HEADER_FIELD_SIGNATURE, g_variant_new_signature ("(tyb)"));
     g_dbus_message_set_body(message, body);
     mbuf = g_dbus_message_to_blob(message,
                          &size,
                          G_DBUS_CAPABILITY_FLAGS_NONE,
                          &error);
-    g_print("Message size %u\n", size);
-    for (int i = 0; i < size; i++) {
-        g_print("\\x%02x", mbuf[i]);
+    if (error == NULL) {
+        g_print("error code NULL\n");
+    }
+    g_print("Message size %lu\n", size);
+    for (guint i = 0; i < size; i++) {
+        g_print("\\\\x%02x", mbuf[i]);
     }
     g_print("\n");
     return 0;
